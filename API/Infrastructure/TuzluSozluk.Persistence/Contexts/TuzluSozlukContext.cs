05ed1a206cf283eb998c1c004ee400e3f7685c2c
Seda Yeler <sedaayeler@gmail.com>
2024-12-15 00:24:16 +0200

    Add TuzluSozlukDbContext

---
+++ API/Infrastructure/TuzluSozluk.Persistence/Contexts/TuzluSozlukContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TuzluSozluk.Domain.Entities;
+using TuzluSozluk.Domain.Entities.Common;
+
+namespace TuzluSozluk.Persistence.Contexts
+{
+    public class TuzluSozlukContext : DbContext
+    {
+        public TuzluSozlukContext()
+        {
+
+        }
+
+        public TuzluSozlukContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Entry> Entries { get; set; }
+        public DbSet<EntryVote> EntryVotes { get; set; }
+        public DbSet<EntryFavorite> EntryFavorites { get; set; }
+        public DbSet<EntryComment> EntryComments { get; set; }
+        public DbSet<EntryCommentVote> EntryCommentVotes { get; set; }
+        public DbSet<EntryCommentFavorite> EntryCommentFavorites { get; set; }
+        public DbSet<EmailConfirmation> EmailConfirmations { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var datas = ChangeTracker.Entries<BaseEntity>();
+
+            foreach (var data in datas)
+            {
+                _ = data.State switch
+                {
+                    EntityState.Added => data.Entity.CreatedAt = DateTime.UtcNow,
+                    _ => DateTime.UtcNow
+                };
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
--- API/Infrastructure/TuzluSozluk.Persistence/obj/Debug/net8.0/TuzluSozluk.Persistence.AssemblyInfo.cs
+++ API/Infrastructure/TuzluSozluk.Persistence/obj/Debug/net8.0/TuzluSozluk.Persistence.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TuzluSozluk.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+450b7daf009679bbd6f24b0c38734e9ea758e392")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df6af91618d3ece99c65a00c5c40359b710aac35")]
 [assembly: System.Reflection.AssemblyProductAttribute("TuzluSozluk.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TuzluSozluk.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
