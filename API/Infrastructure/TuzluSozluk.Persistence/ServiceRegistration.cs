73654f9058a5b64c7409a99dbbe7fc389fa5d02b
Seda Yeler <sedaayeler@gmail.com>
2024-12-17 18:44:30 +0200

    Add services to DI container

--- API/Infrastructure/TuzluSozluk.Persistence/ServiceRegistration.cs
+++ API/Infrastructure/TuzluSozluk.Persistence/ServiceRegistration.cs
@@ -6,7 +6,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TuzluSozluk.Application.Repositories;
 using TuzluSozluk.Persistence.Contexts;
+using TuzluSozluk.Persistence.Repositories;
 
 namespace TuzluSozluk.Persistence
 {
@@ -15,6 +17,16 @@ namespace TuzluSozluk.Persistence
         public static void AddPersistenceService(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<TuzluSozlukContext>(options => options.UseNpgsql(configuration.GetConnectionString("PostgreSQL")));
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IEmailConfirmationRepository, EmailConfirmationRepository>();
+
+            services.AddScoped<IEntryRepository, EntryRepository>();
+            services.AddScoped<IEntryVoteRepository, EntryVoteRepository>();
+            services.AddScoped<IEntryFavoriteRepository, EntryFavoriteRepository>();
+
+            services.AddScoped<IEntryCommentRepository, EntryCommentRepository>();
+            services.AddScoped<IEntryCommentVoteRepository, EntryCommentVoteRepository>();
+            services.AddScoped<IEntryCommentFavoriteRepository, EntryCommentFavoriteRepository>();
         }
     }
 }
